b3ab2fd9153fced50b28f625f5ba56f759b18868
Fernando Crozetta <fernando@czetta.com>
2023-03-10 23:54:36 +0100

    cleaning up the project, to get ready for new implementation

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-NEO4J_URI = os.environ.get("NEO4J_URI", "bolt://localhost:7687")
-NEO4J_DB = os.environ.get("NEO4J_DB", "neo4j")
-NEO4J_USER = os.environ.get("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD", "neo4j")
